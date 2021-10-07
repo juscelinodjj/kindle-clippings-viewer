9830f6c176e1c18d9c4151fb1c986ba494af6f62
Juscelino <juscelinodjj@gmail.com>
2021-10-07 13:03:06 -0300

    Update cache service worker

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // https://developers.google.com/codelabs/pwa-training/pwa03--going-offline
 
 // Choose a cache name
-const cacheName = 'cache-28-09-2021-c';
+const cacheName = 'cache-07-10-2021';
 
 // List the files to precache
 const precacheResources = [
